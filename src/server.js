c4e5cdec75fc03686dd3488cc72af50bc0d46f3f
sw-song <sw930601@gmail.com>
2022-02-02 23:01:17 +0900

    p2p chat clear

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
+import { handle } from "express/lib/application";
+import { SocketAddress } from "net";
 
 const app = express();
 
@@ -12,5 +16,31 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log('listening on http://localhost:3000');
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.listen(3000, handleListen);
+const sockets = [];
+
+/* connection to browser */
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "anonymous";
+    console.log("Connected to Browser ^^");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser -.-");
+    });
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket['nickname'] = message.payload;
+                break;
+        }
+    });
+})
+
+
+server.listen(3000, handleListen);
